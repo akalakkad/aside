5c06addf76a0e3efbb3801b8eb3275fac440cbcb
akalakkad <advait.kalakkad@gmail.com>
2020-06-12 23:46:24 -0700

    removed vuex functionality

--- src/store/index.js
+++ src/store/index.js
@@ -5,61 +5,11 @@ Vue.use(Vuex);
 
 const store = new Vuex.Store({
     state: {
-        snippet: '',
-        asides: [
-            {
-                title: 'My Sheet',
-                hash: 'hgFHgc',
-                body: '',
-                parent: [],
-                child: []
-            }
-        ],
-        current: 'hgFHgc',
-        currentSheet: "mysheet",
-        local: ''
+        snippet: ''
+        currentSheet: "mysheet"
     },
     mutations: {
-        update(state, s) {
-            state.snippet = s;
-        },
-        addAside(state, h) {
-            let r = Math.random().toString(36).substring(7);
-            let parent = state.asides.find(element => {
-                return element.hash === h;
-            });
 
-            let aside = {
-                title: state.snippet,
-                hash: r,
-                body: '',
-                parent: [{title: parent.title, hash: parent.hash}],
-                child: []
-            }
-
-            parent.child.push({title: aside.title, hash: aside.hash});
-            
-            state.asides.push(aside);
-            state.current = aside.hash;
-            state.local = aside.body;
-            
-            console.log(state.asides);
-        },
-        saveSheet(state, payload) {
-            let cIdx = state.asides.findIndex(element => {
-                return element.hash === payload.h;
-            });
-            state.asides[cIdx].body = payload.b;
-            state.asides[cIdx].title = payload.t;
-        },
-        getSheet(state, h) {
-            let s = state.asides.find(element => {
-                return element.hash === h;
-            });
-
-            console.log(s);
-            return s;
-        }
     }
 })
 
